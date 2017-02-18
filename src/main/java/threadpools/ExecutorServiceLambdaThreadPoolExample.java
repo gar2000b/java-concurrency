8605be78c6070da95d358f05aad199a173857df3
Gary Black <gar2000b@yahoo.com>
2017-02-18 16:53:29 +0000

    Adding Thread Pool example with Lambda expression.

---
+++ src/main/java/threadpools/ExecutorServiceLambdaThreadPoolExample.java
@@ -0,0 +1,53 @@
+package threadpools;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Offers similar functionality to the
+ * ExecutorServiceCachedThreadPoolExample.
+ * 
+ * This time, we don't use the Runnable processor but a simple method
+ * testLamba() to act as our process. The Lambda expression basically
+ * provides a syntactic reduction of an anonymous implementation of
+ * the Runnable Interface.
+ * 
+ * In other words, it turns testLambda() into a Runnable type under the
+ * hood. Extremely powerful.
+ * 
+ * @author Digilogue on 18/02/17
+ *
+ */
+public class ExecutorServiceLambdaThreadPoolExample {
+
+	public ExecutorServiceLambdaThreadPoolExample() {
+
+		ExecutorService executor = Executors.newCachedThreadPool();
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("i is: " + i);
+			int j = i;
+			executor.submit(() -> testLambda(j));
+		}
+
+		executor.shutdown();
+		System.out.println("All tasks submitted");
+
+		try {
+			executor.awaitTermination(1, TimeUnit.DAYS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("All tasks completed.");
+	}
+
+	public static void main(String[] args) {
+		new ExecutorServiceLambdaThreadPoolExample();
+	}
+
+	public static void testLambda(int id) {
+		System.out.println("Testing out Lambda expression from executor with id: " + id);
+	}
+}
